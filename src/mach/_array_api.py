94164f2c9bc488485a15b49bdb32d46505e89cb9
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2025-07-17 14:33:54 -0700

    extra array protocol

--- src/mach/_array_api.py
+++ src/mach/_array_api.py
@@ -19,8 +19,9 @@ class DLPackDevice(int, Enum):
 class Array(Protocol):
     """Protocol for arrays that conform to the Array API standard.
 
-    This is a lightweight implementation that will eventually be replaced by
-    one of the following:
+    This is a lightweight implementation that covers the basic operations
+    needed by the mach codebase.  It will eventually be replaced by one of the
+    following:
     https://github.com/magnusdk/spekk/commit/d17d5bbd3e2beac97142a9397ce25942b787a7ed
     https://github.com/data-apis/array-api/pull/589/
     https://github.com/data-apis/array-api-typing
@@ -30,3 +31,14 @@ class Array(Protocol):
     shape: tuple[int, ...]
 
     def __dlpack_device__(self) -> tuple[int, int]: ...
+
+    # Basic operations used in the codebase
+    def __add__(self, other: Any) -> "Array": ...
+    def __sub__(self, other: Any) -> "Array": ...
+    def __truediv__(self, other: Any) -> "Array": ...
+    def __getitem__(self, key: Any) -> "Array": ...
+    def reshape(self, shape: Any) -> "Array": ...
+    def astype(self, dtype: Any, *, copy: bool = True) -> "Array": ...
+
+    @property
+    def T(self) -> "Array": ...
